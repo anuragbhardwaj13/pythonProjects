7eede802882e7255ce63421c2d87fefae4f070b5
Anurag Bhardwaj <a4anurag13@gmail.com>
2022-10-21 21:20:23 +0530

    day2- completed

---
+++ Day2/main.py
@@ -0,0 +1,16 @@
+var=3000
+print(f"Hello {var},{var}*{var}")
+a=7
+b=2
+print(int(a/b))
+print(float(7/2))
+print(783**0.5)
+print(51.5/3)
+print(100/3)
+print(round(90/7))
+print(type(7.5 + 2.5))
+
+
+
+
+
---
+++ Day2/saleBouns.py
@@ -0,0 +1,4 @@
+name=input("What's your Name : ")
+value=round(float(input("Value of total sales made this month : ")),2)
+bonus=round(value*0.13,2)
+print(f"Confratulations Mr.{name}\nYour Bonus amount for this month is {bonus}")
